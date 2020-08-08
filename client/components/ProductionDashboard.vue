b5d746bece2826ae94c29b7161f28d81afda9385
stevemeister5 <stephenreubenm@gmail.com>
2020-08-08 11:02:39 +0300

    Production Dashboard Script Setup

---
+++ client/components/ProductionDashboard.vue
@@ -0,0 +1,25 @@
+<script>
+
+import { Measures } from '../../lib/collections'
+
+export default {
+
+    data () {
+        return {
+            measures: [],
+        }
+    },
+    
+    meteor: {
+        $subscribe: {
+            'measures': [],
+        },
+
+        measures () {
+            return Measures.find({}, {
+                sort: { date: -1 },
+            })
+        },
+    },
+}
+</script>
