ec6853a93d118d0a41282da17979bf5245cb3f1f
stevemeister5 <stephenreubenm@gmail.com>
2020-08-08 11:15:15 +0300

    Indicators Setup

--- client/components/ProductionDashboard.vue
+++ client/components/ProductionDashboard.vue
@@ -1,3 +1,25 @@
+<template>
+    <div class="production-dashboard">
+        <h1>Production Dashboard</h1>
+
+        <section class="indicators">
+            <ProductionIndicator
+                :value="average / 100"
+                title="Average"
+                :info="Math.round(average)"
+            />
+            <ProductionIndicator
+                class="danger"
+                :value="errorRate"
+                title="Errors"
+                :info="`${Math.round(errorRate * 100) }%`"
+            />
+        </section>
+    </div>
+</template>
+
+
+
 <script>
 
 import { Measures } from '../../lib/collections'
@@ -9,7 +31,7 @@ export default {
             measures: [],
         }
     },
-    
+
     meteor: {
         $subscribe: {
             'measures': [],
@@ -19,6 +41,30 @@ export default {
             return Measures.find({}, {
                 sort: { date: -1 },
             })
+        },
+    },
+
+    computed: {
+        length () {
+            return this.measures.length
+        },
+
+        average () {
+            if (!this.length) return 0
+            let total = this.measures.reduce(
+                (total, measure) => total += measure.value,
+                0
+            )
+            return total / this.length
+        },
+
+        errorRate () {
+            if (!this.length) return 0
+            let total = this.measure.reduce(
+                (total, measure) => total += measure.error ? 1 : 0,
+                0
+            )
+            return total / this.length
         },
     },
 }
