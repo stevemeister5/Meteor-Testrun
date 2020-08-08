2a73e9983480b587e9c699ab053dcdc6dd2e1ea8
stevemeister5 <stephenreubenm@gmail.com>
2020-08-08 10:58:05 +0300

    Progress Bars Setup

--- client/main.js
+++ client/main.js
@@ -2,10 +2,16 @@ import { Meteor } from 'meteor/meteor'
 import Vue from 'vue'
 import App from './components/App.vue'
 import VueMeteorTracker from 'vue-meteor-tracker'
+import 'vue-progress-path/dist/vue-progress-path.esm'
+import VueProgress from 'vue-progress-path'
 
 import router from './router'
 
 Vue.use(VueMeteorTracker)
+
+Vue.use(VueProgress, {
+    defaultShape: 'semicircle',
+})
 
 Meteor.startup( () => {
     new Vue({
