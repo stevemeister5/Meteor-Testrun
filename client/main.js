2a9345266d1c6959ec3731a5b7240229ed1cf38b
stevemeister5 <stephenreubenm@gmail.com>
2020-08-08 10:43:18 +0300

    Collections Setup

--- client/main.js
+++ client/main.js
@@ -1,8 +1,11 @@
 import { Meteor } from 'meteor/meteor'
 import Vue from 'vue'
 import App from './components/App.vue'
+import VueMeteorTracker from 'vue-meteor-tracker'
 
 import router from './router'
+
+Vue.use(VueMeteorTracker)
 
 Meteor.startup( () => {
     new Vue({
